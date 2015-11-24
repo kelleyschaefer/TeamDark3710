23a9b924e43b4ad50d5a0bb7acab7b7c5fce3637
MichaelJames16 <m.harrelson86@gmail.com>
2015-11-23 21:52:59 -0700

    More framework for level select
    Added in the main GUI menu for level select and deactivated the main
    menu buttons while level select GUI is up.

--- CodenameNycto/Assets/Scripts/GameManager.cs
+++ CodenameNycto/Assets/Scripts/GameManager.cs
@@ -4,63 +4,96 @@ using System.Collections.Generic;
 public class GameManager : MonoBehaviour {
 
 	public GameObject Level1Button;
+	public Rect LevelSelectWindow;
+	public GameObject play;
+	public GameObject levelSelect;
+	public GameObject exit;
 
-	private List<string> UnlockedLevels;
+	private bool hasFocus;
+	private bool toggleWindow;
+	private float width;
+	private List<string> unlockedLevels;
+	private Rect levelSelectWindow;
 
 	// Use this for initializations
 	void Start () {
-		UnlockedLevels = new List <string>();
-		UnlockedLevels.Add("PrototypeLevel");
-		UnlockedLevels.Add("Level1");
+		unlockedLevels = new List <string>();
+		unlockedLevels.Add("PrototypeLevel");
+		unlockedLevels.Add("Level1");
 
-		LevelSelectPopulate();
+		toggleWindow = false;
+		hasFocus = false;
+
+		//play = GetComponent<ButtonObject>();
+
+		width = Screen.width;
+		levelSelectWindow = new Rect((width/2 - ((width/2f))/2f),(Screen.height/10),width/2f,width/1.5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
-	public void LevelSelectPopulate()
-	{
-		foreach (string s in UnlockedLevels)
-		{
+	public void LevelSelect(){
+		toggleWindow = true;
+		disableButtons();
+	}
+
+	private void disableButtons(){
+		play.SetActive(false);
+		levelSelect.SetActive(false);
+		exit.SetActive(false);
+	}
+
+	private void enableButtons(){
+		play.SetActive(true);
+		levelSelect.SetActive(true);
+		exit.SetActive(true);
+	}
+
+	void OnGUI(){
+		if(toggleWindow){
+			levelSelectWindow = GUI.Window (0, levelSelectWindow, window, "Level Select");
+		}
+	}
+
+	void window(int windowID) {
+		//draw the window
+	}
+
+	public void LevelSelectPopulate(){
+		foreach (string s in unlockedLevels){
 
 		}
 	}
 
-	public void RestartLevel()
-	{
+	public void RestartLevel(){
 		Application.LoadLevel(Application.loadedLevel);
 	}
 
-	public void ExitLevel()
-	{
+	public void ExitLevel(){
 		PlayerPrefs.SetInt ("Checkpoint", 0);
 		Application.LoadLevel("MainMenu");
 	}
 
-	public void NewGame()
-	{
+	public void NewGame(){
 		Application.LoadLevel("Level1");
 	}
 
-	public void ExitGame()
-	{
+	public void ExitGame(){
 		PlayerPrefs.SetInt ("Checkpoint", 0);
 		Application.Quit();
 	}
 
-	public void SaveExit()
-	{
+	public void SaveExit(){
 		/*
 		 * TODO: Save file */
 		PlayerPrefs.SetInt ("Checkpoint", 0);
 		Application.Quit ();
 	}
 
-	public void SaveContinue()
-	{
+	public void SaveContinue(){
 		/*
 		 * TODO: Save file */
 		int level = Application.loadedLevel;
@@ -72,8 +105,7 @@ public class GameManager : MonoBehaviour {
 		Application.LoadLevel(level);
 	}
 
-	public void SaveMenu()
-	{
+	public void SaveMenu(){
 		/*
 		 * TODO: Save file */
 		PlayerPrefs.SetInt ("Checkpoint", 0);
